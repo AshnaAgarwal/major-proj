79505bd359948edaa2938cd83ac83d1ae5b4a06c
AshnaAgarwal <64163352+AshnaAgarwal@users.noreply.github.com>
2020-12-02 00:22:10 +0530

    amend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'membership-app';
+
+  onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile();
+    console.log(profile.getName());    
+  }
+
+  clicked() {
+    alert('Logged in as: ');
+  }
 }
